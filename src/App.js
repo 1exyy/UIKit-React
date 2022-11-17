bc028154fb1983aee8ed666bb700dce2b7e6ebd8
1exyy <alxvagbar@gmail.com>
2022-11-17 23:27:41 +0300

    to main

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import File from "./components/UI/File/File";
 import Frame from "./components/Frame/Frame";
 import Popup from "./components/UI/MAC/Popup/Popup";
 import Button from "./components/UI/Button/Button";
+import Sidebar from "./components/Sidebar/Sidebar";
 
 function App() {
     const [isOpen, setIsOpen] = useState(false);
@@ -26,78 +27,79 @@ function App() {
 
     return (
         <div className="App">
-            <Frame className="frame_1">
-                <div className="components">
-                    <div className="component">
-                        <div className="description">
-                            <span>
-                                Компонент Select необходим для выбора одного параметра
-                                Принимает в себя placeholder, который будет указан до выбора какого либо параметра,
-                                cлушатель onSelect и набор опций массив объектов с полями text и value
-                            </span>
-                        </div>
-                        <Select placeholder='Выбор' onSelect={selectHandler} options={[
-                            {text: 'Параметр 1', value: "М"},
-                            {text: 'Параметр 2', value: "Ж"},
-                        ]}/>
-                    </div>
-                    <div className="component">
-                        <div className="description">
-                             <span>
-                               Компонент Input служит для ввода каких либо текстовых данных
-                                 Принимает в себя один параметр - placeholder
-                             </span>
-                        </div>
-                        <Input placeholder='Имя'/>
-                    </div>
+            <Sidebar/>
+            {/*<Frame className="frame_1">*/}
+            {/*    <div className="components">*/}
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                <span>*/}
+            {/*                    Компонент Select необходим для выбора одного параметра*/}
+            {/*                    Принимает в себя placeholder, который будет указан до выбора какого либо параметра,*/}
+            {/*                    cлушатель onSelect и набор опций массив объектов с полями text и value*/}
+            {/*                </span>*/}
+            {/*            </div>*/}
+            {/*            <Select placeholder='Выбор' onSelect={selectHandler} options={[*/}
+            {/*                {text: 'Параметр 1', value: "М"},*/}
+            {/*                {text: 'Параметр 2', value: "Ж"},*/}
+            {/*            ]}/>*/}
+            {/*        </div>*/}
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                 <span>*/}
+            {/*                   Компонент Input служит для ввода каких либо текстовых данных*/}
+            {/*                     Принимает в себя один параметр - placeholder*/}
+            {/*                 </span>*/}
+            {/*            </div>*/}
+            {/*            <Input placeholder='Имя'/>*/}
+            {/*        </div>*/}
 
-                    <div className="component">
-                        <div className="description">
-                             <span>
-                               Компонент Input служит для ввода каких либо текстовых данных
-                                 Принимает в себя один параметр - placeholder
-                             </span>
-                        </div>
-                        <Email/>
-                    </div>
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                 <span>*/}
+            {/*                   Компонент Input служит для ввода каких либо текстовых данных*/}
+            {/*                     Принимает в себя один параметр - placeholder*/}
+            {/*                 </span>*/}
+            {/*            </div>*/}
+            {/*            <Email/>*/}
+            {/*        </div>*/}
 
-                    <div className="component">
-                        <div className="description">
-                            <span>Компонент Date служит для выбора даты</span>
-                        </div>
-                        <Date/>
-                    </div>
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                <span>Компонент Date служит для выбора даты</span>*/}
+            {/*            </div>*/}
+            {/*            <Date/>*/}
+            {/*        </div>*/}
 
-                    <div className="component">
-                        <div className="description">
-                            <span>Компонент Password служит для ввода пароля</span>
-                        </div>
-                        <Password/>
-                    </div>
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                <span>Компонент Password служит для ввода пароля</span>*/}
+            {/*            </div>*/}
+            {/*            <Password/>*/}
+            {/*        </div>*/}
 
 
-                    <div className="component">
-                        <div className="description">
-                            <span>Компонент File служит для получения файла от пользователя, так же работает drop файла</span>
-                        </div>
-                        <File/>
-                    </div>
+            {/*        <div className="component">*/}
+            {/*            <div className="description">*/}
+            {/*                <span>Компонент File служит для получения файла от пользователя, так же работает drop файла</span>*/}
+            {/*            </div>*/}
+            {/*            <File/>*/}
+            {/*        </div>*/}
 
-                    <Checkbox onChange={checkBoxHandler}/>
-                    <Radio onChange={checkBoxHandler}/>
-                    <Switch/>
+            {/*        <Checkbox onChange={checkBoxHandler}/>*/}
+            {/*        <Radio onChange={checkBoxHandler}/>*/}
+            {/*        <Switch/>*/}
 
-                </div>
-            </Frame>
+            {/*    </div>*/}
+            {/*</Frame>*/}
 
-            <Frame className="frame_1">
-                <Button onClick={()=>setIsOpen(true)}>
-                    Жимааай
-                </Button>
-                <Popup title='Увага!' state={isOpen} stateFx={setIsOpen}>
-                    По рзелульаттам илссеовадний одонго анлигйсокго унвиертисета, не иеемт занчнеия, в кокам пряокде рсапожолены бкувы в солве. Галвоне, чотбы преавя и пслоендяя бквуы блыи на мсете. Осатьлыне бкувы мгоут селдовтаь в плоонм бсепордяке, все-рвано ткест чтаитсея без побрелм. Пичрионй эгото ялвятеся то, что мы чиатем не кдаужю бкуву по отдльенотси, а все солво цликеом.
-                </Popup>
-            </Frame>
+            {/*<Frame className="frame_1">*/}
+            {/*    <Button onClick={()=>setIsOpen(true)}>*/}
+            {/*        Жимааай*/}
+            {/*    </Button>*/}
+            {/*    <Popup title='Увага!' state={isOpen} stateFx={setIsOpen}>*/}
+            {/*        По рзелульаттам илссеовадний одонго анлигйсокго унвиертисета, не иеемт занчнеия, в кокам пряокде рсапожолены бкувы в солве. Галвоне, чотбы преавя и пслоендяя бквуы блыи на мсете. Осатьлыне бкувы мгоут селдовтаь в плоонм бсепордяке, все-рвано ткест чтаитсея без побрелм. Пичрионй эгото ялвятеся то, что мы чиатем не кдаужю бкуву по отдльенотси, а все солво цликеом.*/}
+            {/*    </Popup>*/}
+            {/*</Frame>*/}
         </div>
     );
 }
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sidebar.css'
+
+const Sidebar = () => {
+    return (
+        <div className='Sidebar'>
+           <div className="burger-btn">
+               <span/>
+           </div>
+        </div>
+    );
+};
+
+export default Sidebar;
